e2fde90c8804c95aafc7fdd771ce66ca84732716
vajen <vs.evg@yandex.ru>
2019-03-04 15:15:57 +0300

    delete comments

--- src/app/uniq-type.pipe.ts
+++ src/app/uniq-type.pipe.ts
@@ -7,19 +7,10 @@ import {IHotels} from "./common/mock/data";
 export class UniqTypePipe implements PipeTransform {
   public _uniqTypes: string[];
 
-  //transform(value: string): string {
-    // TODO
-    /*if (!this._uniqTypes.includes(value)) {
-    // if (this._uniqTypes.indexOf(value) < 0) {
-      this._uniqTypes.push(value);
-      return value;
-    }*/
     transform(hotels: IHotels[]): string[] {
       const types = hotels.map((hotel)=>{
         return hotel.type;
       });
       return Array.from(new Set(types));
     }
-  //}
-
 }
